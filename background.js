20ccbcaa1abeac2a87bc90d3e1d3d146b30b8fb1
kaydee <kaydey287@gmail.com>
2019-02-14 22:33:36 +0530

    Working on its own

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-  chrome.declarativeContent.onPageChanged.addRules([{
-    conditions: [new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: {hostEquals: 'github.com'},
-    })
-    ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-  }]);
+chrome.runtime.onInstalled.addListener (function (details) {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostEquals: 'github.com', schemes: ['https']},
+      })
+      ],
+          actions: [
+            // new chrome.declarativeContent.ShowPageAction(),
+            new chrome.declarativeContent.RequestContentScript({"js": ['index.js']})
+          ]
+    }]);
+  });
 });
